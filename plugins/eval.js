73a6a0a885abfc5aa88fde54d3a3f9d4f1accf8e
Lynx <risvanlynx777@gmail.com>
2025-04-27 15:03:46 +0530

    Update eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -45,14 +45,13 @@ const config = require("../config");
 /* Copyright (C) 2024 QUEEN NEZUKO.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-QUEEN NEZUKO- GOD ZENITSU
 */
 
 command({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
-  if (match.startsWith("$")) {
+  if (match.startsWith(">")) {
     //const m = message;
     try {
-      let evaled = await eval(`${match.replace("$", "")}`);
+      let evaled = await eval(`${match.replace(">", "")}`);
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
       await message.reply(evaled);
     } catch (err) {
