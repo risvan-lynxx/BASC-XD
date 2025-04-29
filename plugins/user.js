5834f7486057e25d4aca7f4fcfc9c4144cffd7aa
Lynx <risvanlynx777@gmail.com>
2025-04-29 11:39:17 +0530

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -80,51 +80,19 @@ X-Asena - X-Electra
 
 command(
   {
-    pattern: "pp$",
+    pattern: "pp",
     fromMe: true,
-    desc: "Set full screen profile picture",
+    desc: "Set profile picture",
     type: "user",
   },
-  async (message, match,m) => {
+  async (message, match, m) => {
     if (!message.reply_message.image)
-      return await message.reply("*_ʀᴇᴩʟᴀʏ ᴛᴏ ᴀ ɪᴍᴀɢᴇ_*");
-    let media = await m.quoted.download();
-    await updateProfilePicture(message.user, media, message);
-    return await message.reply("*_ᴩʀᴏꜰɪʟᴇ ᴩɪᴄᴛᴜʀᴇ ᴜᴩᴅᴀᴛᴇᴅ_*");
+      return await message.reply("_Reply to a photo_");
+    let buff = await m.quoted.download();
+    await message.setPP(message.user, buff);
+    return await message.reply("_Profile Picture Updated_");
   }
 );
-
-async function updateProfilePicture(jid, imag, message) {
-  const { query } = message.client;
-  const { img } = await generateProfilePicture(imag);
-  await query({
-    tag: "iq",
-    attrs: {
-      to: jid,
-      type: "set",
-      xmlns: "w:profile:picture",
-    },
-    content: [
-      {
-        tag: "picture",
-        attrs: { type: "image" },
-        content: img,
-      },
-    ],
-  });
-}
-
-async function generateProfilePicture(buffer) {
-  const jimp = await Jimp.read(buffer);
-  const min = jimp.getWidth();
-  const max = jimp.getHeight();
-  const cropped = jimp.crop(0, 0, min, max);
-  return {
-    img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
-    preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
-  };
-}
-
 
 
 command(
